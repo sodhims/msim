5b2277611fe6ac7e2a635220e2ff567eed9e4d5d
sodhims <sodhi@egr.uri.edu>
2025-10-29 23:39:38 -0400

    Works (I think)

--- ManufacturingSimulation.Core/SimulationEngine.cs
+++ ManufacturingSimulation.Core/SimulationEngine.cs
@@ -23,6 +23,21 @@ namespace ManufacturingSimulation.Core
         public bool IsRunning { get; private set; }
         public event EventHandler<SimulationEvent>? EventProcessed;
 
+        private StreamWriter _debugLog;
+
+        //public SimulationEngine(int randomSeed = 42)
+        //{
+        //    _scheduler = new EventScheduler();
+        //    _random = new Random(randomSeed);
+        //    _currentTime = 0;
+        //    Machines = new List<Machine>();
+        //    Buffers = new Dictionary<int, MachineBuffer>();
+        //    _totalPartsArrived = 0;
+        //    _totalPartsCompleted = 0;
+        //    _completedParts = new List<Part>();
+        //    _machineBusyTime = new Dictionary<int, double>();
+        //    Logger = new SimulationLogger();
+        //}
         public SimulationEngine(int randomSeed = 42)
         {
             _scheduler = new EventScheduler();
@@ -35,6 +50,18 @@ namespace ManufacturingSimulation.Core
             _completedParts = new List<Part>();
             _machineBusyTime = new Dictionary<int, double>();
             Logger = new SimulationLogger();
+
+            // Create debug log file
+            string debugPath = @"C:\msim\logs\debug.txt";
+            Directory.CreateDirectory(@"C:\msim\logs");
+            _debugLog = new StreamWriter(debugPath, false); // false = overwrite
+            _debugLog.AutoFlush = true;
+            _debugLog.WriteLine($"=== Simulation Started at {DateTime.Now} ===\n");
+        }
+
+        private void Debug(string message)
+        {
+            _debugLog?.WriteLine($"[{_currentTime:F2}] {message}");
         }
 
         public void AddMachine(Machine machine, int bufferCapacity)
@@ -64,57 +91,121 @@ namespace ManufacturingSimulation.Core
             IsRunning = false;
         }
 
-        public void HandlePartArrival(Part part)
-        {
-            _totalPartsArrived++;
-            int firstMachineId = part.GetCurrentMachineId();
-            var buffer = Buffers[firstMachineId];
-            Logger.LogPartArrival(_currentTime, part, firstMachineId);
-            bool added = buffer.TryAdd(part, _currentTime);
-            if (!added) return;
-            var machine = Machines.First(m => m.Id == firstMachineId);
-            TryStartProcessing(machine);
-        }
+        //public void HandlePartArrival(Part part)
+        //{
+        //    _totalPartsArrived++;
+        //    int firstMachineId = part.GetCurrentMachineId();
+        //    var buffer = Buffers[firstMachineId];
+        //    Logger.LogPartArrival(_currentTime, part, firstMachineId);
+        //    bool added = buffer.TryAdd(part, _currentTime);
+        //    if (!added) return;
+        //    var machine = Machines.First(m => m.Id == firstMachineId);
+        //    TryStartProcessing(machine);
+        //}
 
+        //public void HandleProcessingComplete(Machine machine, Part part)
+        //{
+        //    TrackMachineBusyTime(machine, machine.ProcessingStartTime, _currentTime);
+        //    Logger.LogPartProcessingComplete(_currentTime, machine, part);
+
+        //    // Move to next operation
+        //    part.MoveToNextOperation();
+
+        //    if (part.HasMoreOperations())
+        //    {
+        //        int nextMachineId = part.GetCurrentMachineId();
+        //        var nextBuffer = Buffers[nextMachineId];
+        //        bool added = nextBuffer.TryAdd(part, _currentTime);
+
+        //        if (!added)
+        //        {
+        //            // BLOCKED - don't clear machine, don't try to start
+        //            machine.State = MachineState.Blocked;
+        //            machine.CurrentPart = part;
+        //            Logger.LogMachineBlocked(_currentTime, machine, part, nextMachineId);
+        //            _scheduler.ScheduleEvent(new RetryTransferEvent(_currentTime + 0.5, machine, part, nextMachineId));
+        //            return;  // EXIT - don't do anything else!
+        //        }
+
+        //        // SUCCESS - transfer complete
+        //        Logger.LogPartTransfer(_currentTime, part, machine.Id, nextMachineId);
+        //        machine.CurrentPart = null;
+        //        machine.State = MachineState.Idle;
+        //        machine.PartsCompleted++;
+
+        //        // Try to start next machine
+        //        TryStartProcessing(Machines.First(m => m.Id == nextMachineId));
+
+        //        // Try to start THIS machine with next part
+        //        TryStartProcessing(machine);  // ? MOVE IT HERE
+        //    }
+        //    else
+        //    {
+        //        // Part completely done
+        //        machine.CurrentPart = null;
+        //        machine.State = MachineState.Idle;
+        //        machine.PartsCompleted++;
+
+        //        part.State = PartState.Completed;
+        //        part.CompletionTime = _currentTime;
+        //        _completedParts.Add(part);
+        //        _totalPartsCompleted++;
+
+        //        double flowTime = part.CompletionTime - part.ArrivalTime;
+        //        Logger.LogPartCompleted(_currentTime, part, flowTime);
+
+        //        // Try to start THIS machine with next part
+        //        TryStartProcessing(machine);  // ? MOVE IT HERE TOO
+        //    }
+
+        //    // ? REMOVE the TryStartProcessing call from here!
+        //}
         public void HandleProcessingComplete(Machine machine, Part part)
         {
+            Debug($"*** {part.Id} COMPLETED on {machine.Name} ***");
+
             TrackMachineBusyTime(machine, machine.ProcessingStartTime, _currentTime);
             Logger.LogPartProcessingComplete(_currentTime, machine, part);
 
-            // Move to next operation
             part.MoveToNextOperation();
+            Debug($"    Moved to operation {part.CurrentOperationIndex}");
 
             if (part.HasMoreOperations())
             {
                 int nextMachineId = part.GetCurrentMachineId();
                 var nextBuffer = Buffers[nextMachineId];
+
+                Debug($"    Next: Machine {nextMachineId}, Buffer: {nextBuffer.Count}/{nextBuffer.Capacity}");
+
                 bool added = nextBuffer.TryAdd(part, _currentTime);
 
                 if (!added)
                 {
-                    // BLOCKED - don't clear machine, don't try to start
+                    Debug($"    *** BLOCKED! {machine.Name} keeping {part.Id} ***");
+
                     machine.State = MachineState.Blocked;
                     machine.CurrentPart = part;
                     Logger.LogMachineBlocked(_currentTime, machine, part, nextMachineId);
                     _scheduler.ScheduleEvent(new RetryTransferEvent(_currentTime + 0.5, machine, part, nextMachineId));
-                    return;  // EXIT - don't do anything else!
+
+                    Debug($"    Scheduled retry at {_currentTime + 0.5:F2}");
+                    return;
                 }
 
-                // SUCCESS - transfer complete
+                Debug($"    SUCCESS! Transferred to Machine {nextMachineId}");
+
                 Logger.LogPartTransfer(_currentTime, part, machine.Id, nextMachineId);
                 machine.CurrentPart = null;
                 machine.State = MachineState.Idle;
                 machine.PartsCompleted++;
 
-                // Try to start next machine
                 TryStartProcessing(Machines.First(m => m.Id == nextMachineId));
-
-                // Try to start THIS machine with next part
-                TryStartProcessing(machine);  // ? MOVE IT HERE
+                TryStartProcessing(machine);
             }
             else
             {
-                // Part completely done
+                Debug($"    ALL OPERATIONS COMPLETE!");
+
                 machine.CurrentPart = null;
                 machine.State = MachineState.Idle;
                 machine.PartsCompleted++;
@@ -127,20 +218,49 @@ namespace ManufacturingSimulation.Core
                 double flowTime = part.CompletionTime - part.ArrivalTime;
                 Logger.LogPartCompleted(_currentTime, part, flowTime);
 
-                // Try to start THIS machine with next part
-                TryStartProcessing(machine);  // ? MOVE IT HERE TOO
+                // At the very end, after everything:
+                Debug($"    About to call TryStartProcessing({machine.Name})");
+                TryStartProcessing(machine);
+                Debug($"    Finished TryStartProcessing({machine.Name})");
             }
 
-            // ? REMOVE the TryStartProcessing call from here!
+        }
+
+        public void HandlePartArrival(Part part)
+        {
+            _totalPartsArrived++;
+            int firstMachineId = part.GetCurrentMachineId();
+            var buffer = Buffers[firstMachineId];
+
+            Debug($"=== {part.Id} ARRIVES at Machine {firstMachineId} buffer ===");
+
+            Logger.LogPartArrival(_currentTime, part, firstMachineId);
+            bool added = buffer.TryAdd(part, _currentTime);
+            if (!added)
+            {
+                Debug($"    REJECTED! Buffer full");
+                return;
+            }
+
+            Debug($"    Buffer now: {buffer.Count}/{buffer.Capacity}");
+
+            var machine = Machines.First(m => m.Id == firstMachineId);
+            TryStartProcessing(machine);
         }
         private void TryStartProcessing(Machine machine)
         {
-            if (!machine.IsAvailable()) return;
+            Debug($"[TryStart] {machine.Name} - State: {machine.State}, Buffer: {Buffers[machine.Id].Count}");
+
+            if (!machine.IsAvailable())
+            {
+                Debug($"[TryStart] {machine.Name} NOT available (state={machine.State})");
+                return;
+            }
             var buffer = Buffers[machine.Id];
             var part = buffer.SelectAndRemove(machine.DispatchingRule, _currentTime);
             if (part == null)
             {
-                machine.State = MachineState.Starved;
+                Debug($"[TryStart] {machine.Name} buffer empty, staying Idle");
                 return;
             }
             double processingTime = 2.0 + _random.NextDouble() * 4.0;
@@ -150,8 +270,31 @@ namespace ManufacturingSimulation.Core
             _scheduler.ScheduleEvent(new ProcessingCompleteEvent(_currentTime + processingTime, machine, part));
         }
 
+        //public void Reset()
+        //{
+        //    _scheduler.Clear();
+        //    _currentTime = 0;
+        //    IsRunning = false;
+        //    _totalPartsArrived = 0;
+        //    _totalPartsCompleted = 0;
+        //    _completedParts.Clear();
+        //    _machineBusyTime.Clear();
+        //    Logger.Clear();
+        //    foreach (var machine in Machines)
+        //    {
+        //        machine.State = MachineState.Idle;
+        //        machine.CurrentPart = null;
+        //    }
+        //    foreach (var buffer in Buffers.Values)
+        //    {
+        //        buffer.Clear();
+        //    }
+        //}
+
         public void Reset()
         {
+            _debugLog?.WriteLine("\n=== RESET CALLED ===\n");
+
             _scheduler.Clear();
             _currentTime = 0;
             IsRunning = false;
@@ -160,17 +303,18 @@ namespace ManufacturingSimulation.Core
             _completedParts.Clear();
             _machineBusyTime.Clear();
             Logger.Clear();
+
             foreach (var machine in Machines)
             {
                 machine.State = MachineState.Idle;
                 machine.CurrentPart = null;
             }
+
             foreach (var buffer in Buffers.Values)
             {
                 buffer.Clear();
             }
         }
-
         private void TrackMachineBusyTime(Machine machine, double startTime, double endTime)
         {
             if (!_machineBusyTime.ContainsKey(machine.Id))
@@ -206,22 +350,65 @@ namespace ManufacturingSimulation.Core
             return stats;
         }
 
+        //public void HandleRetryTransfer(Machine machine, Part part, int targetMachineId)
+        //{
+        //    if (machine.State != MachineState.Blocked) return;
+
+        //    var targetBuffer = Buffers[targetMachineId];
+        //    bool added = targetBuffer.TryAdd(part, _currentTime);
+
+        //    if (added)
+        //    {
+        //        // SUCCESS - the blocked part finally transferred!
+        //        Logger.LogPartTransfer(_currentTime, part, machine.Id, targetMachineId);  // ? ADD THIS!
+
+        //        machine.CurrentPart = null;
+        //        machine.State = MachineState.Idle;
+        //        machine.PartsCompleted++;  // ? ADD THIS! (part completed processing before blocking)
+
+        //        TryStartProcessing(machine);
+        //        TryStartProcessing(Machines.First(m => m.Id == targetMachineId));
+        //    }
+        //    else
+        //    {
+        //        _scheduler.ScheduleEvent(new RetryTransferEvent(_currentTime + 0.5, machine, part, targetMachineId));
+        //    }
+        //}
         public void HandleRetryTransfer(Machine machine, Part part, int targetMachineId)
         {
-            if (machine.State != MachineState.Blocked) return;
+            Debug($">>> RETRY: {part.Id} from {machine.Name} to Machine {targetMachineId}");
+            Debug($"    Machine state: {machine.State}");
+
+            if (machine.State != MachineState.Blocked)
+            {
+                Debug($"    NOT BLOCKED - Skipping");
+                return;
+            }
+
             var targetBuffer = Buffers[targetMachineId];
+            Debug($"    Target buffer: {targetBuffer.Count}/{targetBuffer.Capacity}");
+
             bool added = targetBuffer.TryAdd(part, _currentTime);
+
             if (added)
             {
+                Debug($"    *** UNBLOCKED! Transfer succeeded! ***");
+
+                Logger.LogPartTransfer(_currentTime, part, machine.Id, targetMachineId);
+
                 machine.CurrentPart = null;
                 machine.State = MachineState.Idle;
+                machine.PartsCompleted++;
+
                 TryStartProcessing(machine);
                 TryStartProcessing(Machines.First(m => m.Id == targetMachineId));
             }
             else
             {
+                Debug($"    Still full - retry at {_currentTime + 0.5:F2}");
                 _scheduler.ScheduleEvent(new RetryTransferEvent(_currentTime + 0.5, machine, part, targetMachineId));
             }
         }
+
     }
 }
